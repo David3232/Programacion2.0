f5edbe6606d2e3c34cf4368a5b575f18e802adf5
ALUMNADO\dafeca <marke181818@gmail.com>
2017-11-09 09:58:27 +0100

    Actualizacion

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ var velocidades = [0,0,0];
 var vel1=document.getElementById("velocidad1");
 var vel2=document.getElementById("velocidad2");
 var vel3=document.getElementById("velocidad3");
+var rtdhtml=document.getElementById("velmed")
+var veltotal=0;
+var rtd=0;
 // Funciones
 function anadirvelocidades() {
-  velocidades[0]=velocidad1;
-  velocidades[1]=velocidad2;
-  velocidades[2]=velocidad3;
+  veltotal=0;
+  velocidades[0]= parseInt(vel1.value);
+  velocidades[1]= parseInt(vel2.value);
+  velocidades[2]= parseInt(vel3.value);
+  for (var i = 0; i < velocidades.length; i++) {
+    veltotal=veltotal+velocidades[i]
+  }
+  rtd=veltotal/3;
+  rtdhtml.value=rtd;
+  console.log(veltotal);
+  console.log(rtd);
 }
-console.log(velocidades);
