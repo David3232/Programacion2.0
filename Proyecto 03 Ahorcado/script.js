a13c7b16b695103d2c83eb0de319e5d27203af5a
ALUMNADO\dafeca <marke181818@gmail.com>
2017-11-10 10:40:45 +0100

    Actu

--- Proyecto 03 Ahorcado/script.js
+++ Proyecto 03 Ahorcado/script.js
@@ -1,7 +1,7 @@
 //Variable
 var palabra0=[];
 var condicion=true;
-var letrauser= prompt("Dame una letra:")
+var cont=0;
 // Array
 var array1 = ["Patron","Peluqueria","Raton","Gafas","Zapatillas"];
 aleatoria = Math.floor(Math.random()*(array1.length));
@@ -11,5 +11,16 @@ console.log(array1);
 // Estructuras de repeticion
 for (var i = 0; i < adivinar.length; i++) {
     palabra0[i]='0';
+}
+while (cont<5) {
+for (var i = 0; i < adivinar.length; i++) {
+  var letrauser =prompt("Dame letra")
+    if (adivinar.charAt(i)==letrauser) {
+      palabra0[i]=adivinar.charAt(i);
+    } else {
+      cont++
+      alert("has fallado")
+    }
+  }
 }
 console.log(palabra0);
