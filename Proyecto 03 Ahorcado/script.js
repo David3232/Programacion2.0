7e8c3ea9d97a617e74493357128c4bdc46ba3e6c
ALUMNADO\dafeca <marke181818@gmail.com>
2017-11-13 13:37:38 +0100

    actu

--- Proyecto 03 Ahorcado/script.js
+++ Proyecto 03 Ahorcado/script.js
@@ -3,24 +3,30 @@ var palabra0=[];
 var condicion=true;
 var cont=0;
 // Array
-var array1 = ["Patron","Peluqueria","Raton","Gafas","Zapatillas"];
+var array1 = ["patron","pez","ratones","gafa","rojo"];
 aleatoria = Math.floor(Math.random()*(array1.length));
 adivinar = array1[aleatoria];
 console.log(adivinar);
-console.log(array1);
 // Estructuras de repeticion
 for (var i = 0; i < adivinar.length; i++) {
     palabra0[i]='0';
 }
-while (cont<5) {
-for (var i = 0; i < adivinar.length; i++) {
-  var letrauser =prompt("Dame letra")
+for (var x = 0; x<adivinar.length || cont<=5; x++) {
+  cont++
+  var letrauser =prompt("Dame letra");
+  var acertado=false;
+  for (var i = 0; i < adivinar.length; i++) {
     if (adivinar.charAt(i)==letrauser) {
       palabra0[i]=adivinar.charAt(i);
+      acertado=true;
     } else {
-      cont++
-      alert("has fallado")
+
     }
+  }
+  if (acertado==true) {
+    alert("has acertado")
+  } else {
+    alert("no")
   }
 }
 console.log(palabra0);
